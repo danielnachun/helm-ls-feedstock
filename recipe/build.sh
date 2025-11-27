0b04d1aa9f1b931fc62eb311c91a37cac4bccdf4
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-26 17:28:41 -0800

    use rattler

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
+go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=license-files
