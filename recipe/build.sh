c60ba86de3c64747bd80d96d8c5d3d0adbe02c36
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-30 16:08:16 -0800

    update version

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w -I ${CC}"
+go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=license-files
