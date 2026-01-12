fb66479fde391cba4c0f67fc1a144a204dad9dfa
Daniel Nachun <daniel.nachun@gmail.com>
2026-01-12 11:46:34 -0800

    try to skip gold

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,19 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+if [[ ${target_platform} == "linux-aarch64" ]]; then
+tee ${BUILD_PREFIX}/bin/cc_shim << EOF
+args=()
+for arg in "\$@"; do
+    if [[ "\$arg" == "-fuse-ld=gold" ]]; then
+        echo "Go build hack: dropping -fuse-ld=gold arg"
+    else
+        args+=("\$arg")
+    fi
+done
+EOF
+export CC=${BUILD_PREFIX}/bin/cc_shim
+fi
+
 go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=license-files
